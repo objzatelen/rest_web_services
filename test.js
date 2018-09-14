48c0a97db62619f8e7bc7700f510bb94779e4ea4
Ksandr <ap_surface@outlook.com>
2018-09-14 15:22:20 +0200

    updating
    now

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 var MongoClient = require('mongodb').MongoClient;
 
-var uri = "mongodb+srv://tamholmlohmat:Sch!ph0l@incubator0-wpeiv.mongodb.net/test";
+var uri = "mongodb+srv://tom:PasswOrd@incubator0-wpeiv.mongodb.net/test";
 MongoClient.connect(uri, function (err, db) {
     db.close();
 });
